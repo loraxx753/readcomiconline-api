f8fb8f076c445e47c230ea91dcafc83c3adc8a03
Pedro Rodrigues <pedro@Ninjabook.xing.hh>
2017-09-08 12:03:52 +0100

    Moved login logic to a session helper

---
+++ src/server/helpers/session_helper.js
@@ -0,0 +1,50 @@
+import express from 'express';
+
+import upstream from '../../lib/upstream';
+import { ROUTES } from '../../lib/constants';
+
+const session = express.Router();
+
+session.post(ROUTES.login, (req, res) => {
+  const unauthorized = () => {
+    res.status(401).json({
+      error: 'UNAUTHORIZED',
+      message: 'Invalid username or password'
+    });
+  };
+
+  if (!!req.body.username && !!req.body.password) {
+    const requestParams = {
+      formData: {
+        username: req.body.username,
+        password: req.body.password,
+        redirect: ''
+      },
+      url: 'http://readcomiconline.to/Login',
+      method: 'POST',
+      headers: {
+        'Referer': 'http://readcomiconline.to/Login',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    };
+
+    upstream.server_request(requestParams)
+      .then((data) => {
+        const cookiesSet = data.response.headers['set-cookie'] || [];
+        const { username, password, 'ASP.NET_SessionId': sessionId } = data.cookies;
+
+        if (!!username && !!password && !!sessionId) {
+          const jwtToken = createJWToken({ username, password, sessionId });
+          res.json({ jwt: jwtToken });
+        }
+        else {
+          unauthorized();
+        }
+      });
+    }
+    else {
+      unauthorized();
+    }
+});
+
+export { session };
--- src/server/index.js
+++ src/server/index.js
@@ -11,8 +11,8 @@ import { users } from './users';
 import { comics } from './comics';
 import { genres } from './genres';
 import { createListRouter } from './helpers/comic_list_router';
+import { session } from './helpers/session_helper';
 import { ROUTES } from '../lib/constants';
-import upstream from '../lib/upstream';
 
 const app = express();
 
@@ -31,48 +31,8 @@ app.use('/artists', authentication, createListRouter('artist'));
 app.use('/writers', authentication, createListRouter('writer'));
 
 // Login/Logout
+app.use('/', session);
 
-app.post(ROUTES.login, (req, res) => {
-  const unauthorized = () => {
-    res.status(401).json({
-      error: 'UNAUTHORIZED',
-      message: 'Invalid username or password'
-    });
-  };
-
-  if (!!req.body.username && !!req.body.password) {
-    const requestParams = {
-      formData: {
-        username: req.body.username,
-        password: req.body.password,
-        redirect: ''
-      },
-      url: 'http://readcomiconline.to/Login',
-      method: 'POST',
-      headers: {
-        'Referer': 'http://readcomiconline.to/Login',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    };
-
-    upstream.server_request(requestParams)
-      .then((data) => {
-        const cookiesSet = data.response.headers['set-cookie'] || [];
-        const { username, password, 'ASP.NET_SessionId': sessionId } = data.cookies;
-
-        if (!!username && !!password && !!sessionId) {
-          const jwtToken = createJWToken({ username, password, sessionId });
-          res.json({ jwt: jwtToken });
-        }
-        else {
-          unauthorized();
-        }
-      });
-    }
-    else {
-      unauthorized();
-    }
-});
 
 // Static paths
 app.use('/covers', express.static('cache/covers'));
